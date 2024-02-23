5a593c97722d0f284a8828616c3b146a10e12f62
senetcord <senetcord@gmail.com>
2024-02-23 17:24:54 +0200

    completed the search box component

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,43 @@
+"use client";
+
+import { RxCross2 } from "react-icons/rx";
+import { BsFillMicFill } from "react-icons/bs";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FormEvent, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+
+  function handleSubmit(e: FormEvent): void {
+    e.preventDefault();
+    router.push(`/search/web?searchTerm=${term}`);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
+    >
+      <input
+        onChange={(e) => setTerm(e.target.value)}
+        value={term}
+        type="text"
+        className="w-full focus:outline-none"
+      />
+      <RxCross2
+        onClick={() => setTerm("")}
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+      />
+      <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 border-l-2 border-gray-300 mr-3 pl-4" />
+      <AiOutlineSearch
+        onClick={handleSubmit}
+        className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+      />
+    </form>
+  );
 };
 export default SearchBox;
