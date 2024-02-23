99485eff94c8a68f3da6d9fabdf344ab8dcc9e0a
senetcord <senetcord@gmail.com>
2024-02-23 20:24:42 +0200

    completed the search header options

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import SearchBox from "./SearchBox";
 import { RiSettings3Line } from "react-icons/ri";
 import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 const SearchHeader = () => {
   return (
@@ -31,6 +32,7 @@ const SearchHeader = () => {
           Sign in
         </button>
       </div>
+      <SearchHeaderOptions />
     </header>
   );
 };
