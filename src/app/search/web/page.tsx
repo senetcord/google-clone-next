a1eadc7b9c90b0bf57959d93df49da917ea300dc
senetcord <senetcord@gmail.com>
2024-02-23 21:01:12 +0200

    added google search api and handle errors

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,4 +1,44 @@
-const WebSearchPage = () => {
-  return <div>WebSearchPage</div>;
+import Link from "next/link";
+
+interface searchParamsProps {
+  searchTerm: string;
+}
+
+const WebSearchPage = async ({
+  searchParams,
+}: {
+  searchParams: searchParamsProps;
+}) => {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  if (!response.ok) throw new Error("Something went wrong");
+  const data = await response.json();
+  const results = data?.items;
+
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">
+          No results found for {searchParams.searchTerm}
+        </h1>
+        <p className="text-lg">
+          Try searching the web or images for something else{" "}
+          <Link href="/" className="text-blue-500">
+            Home
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {results &&
+        results.map((result: any, idx: number) => {
+          return <h1 key={idx}>{result.title}</h1>;
+        })}
+    </div>
+  );
 };
 export default WebSearchPage;
